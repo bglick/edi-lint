bf44b9f83cb8b7061a7fe64adb605f2ef06942ce
Brian Glick <brian@brian-glick.com>
2014-12-27 15:22:01 +0000

    cleanup formatting

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var ediLint = angular.module('ediLint',[]);
 ediLint.factory('ediParser',[function() {
   return {
     parse: function(txt) {
-      var r, rawSegments, i, curSeg, rawElements, elementObjects, sTypes=Object.create(null);
+      var r, rawSegments, i, curSeg, rawElements, elementObjects, sTypes=Object.create(null), seg;
       r = {errors:[],segments:[],segmentTypes:[]};
-      if(txt==undefined || txt.length==0) {
+      if(txt===undefined || txt.length===0) {
         return r;
       }
       if(!txt.match(/^ISA.{102}/)) {
@@ -28,18 +28,18 @@ ediLint.factory('ediParser',[function() {
           r.segmentTerminatorDesc = "[CR][LF]";
           break;
         default:
-          r.segmentTerminatorDesc = r.segmentTerminator
+          r.segmentTerminatorDesc = r.segmentTerminator;
           break;
       }
       rawSegments = txt.split(r.segmentTerminator);
-      r.segments = []
+      r.segments = [];
       for(i=0;i<rawSegments.length;i++) {
         if(rawSegments[i].trim().length>0) {
           rawElements = rawSegments[i].split(r.elementSeparator);
-          elementObjects = new Array(rawElements.length)
+          elementObjects = new Array(rawElements.length);
           for (var j = rawElements.length - 1; j >= 0; j--) {
             elementObjects[j] = {content:rawElements[j]};
-          };
+          }
           seg = {row:i+1,elements:elementObjects,separator:r.elementSeparator};
           seg.type = seg.elements[0].content;
           r.segments.push(seg);
@@ -49,7 +49,7 @@ ediLint.factory('ediParser',[function() {
       for(var prop in sTypes) {
         r.segmentTypes.push(prop);
       }
-      r.segmentTypes.sort()
+      r.segmentTypes.sort();
       return r;
     }
   };
@@ -74,21 +74,21 @@ ediLint.controller('EdiCtrl',['$scope','ediParser',function($scope,ediParser) {
   $scope.zoomSegment = {};
   $scope.setZoomSegment = function(s) {
     $scope.zoomSegment = s;
-  }
+  };
 
   $scope.toggleSegmentHide = function(t) {
     $scope.hiddenSegmentTypes[t] = !$scope.hiddenSegmentTypes[t];
-  }
+  };
   $scope.showAllSegments = function() {
     $scope.hiddenSegmentTypes = {};
-  }
+  };
   $scope.hideAllSegments = function() {
     var segs = $scope.ediOut.segments;
     var i;
     for(i=0;i<segs.length;i++) {
       $scope.hiddenSegmentTypes[segs[i].type] = true;
     }
-  }
+  };
 
   $scope.$watch('ediIn',function(nv,ov) {
     $scope.ediOut = ediParser.parse(nv);
