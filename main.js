775ec67473f7eb786ba206ca956f3b049b4340ce
Chas. J. Owens IV <chas.owens@gmail.com>
2016-12-13 17:55:32 -0500

    Add error checking of the envelope
    Add error checking for the structural parts that are always the same for an EDI document.

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ ediLint.factory('ediParser',[function() {
       } else {
         r.segmentTerminator = txt[105];
       }
+      if (r.segmentTerminator !== txt[ txt.length - 1 ]) {
+        r.errors.push("Either something is wrong with your ISA or your IEA is missing its terminator");
+        return r;
+      }
       switch(r.segmentTerminator) {
         case "\r":
           r.segmentTerminatorDesc = "[CR]";
@@ -33,6 +37,12 @@ ediLint.factory('ediParser',[function() {
       }
       rawSegments = txt.split(r.segmentTerminator);
       r.segments = [];
+      var transactionSegmentCount = 0;
+      var groupCount              = 0;
+      var isaCount                = 0;
+      var transactionNum          = "";
+      var groupNum                = "";
+      var isaNum                  = "";
       for(i=0;i<rawSegments.length;i++) {
         if(rawSegments[i].trim().length>0) {
           rawElements = rawSegments[i].split(r.elementSeparator);
@@ -42,6 +52,76 @@ ediLint.factory('ediParser',[function() {
           }
           seg = {row:i+1,elements:elementObjects,separator:r.elementSeparator};
           seg.type = seg.elements[0].content;
+          switch (seg.type) {
+            case "ISA":
+              if (isaNum !== "") {
+                r.errors.push("more than one ISA segment");
+              }
+              isaNum = seg.elements[13].content;
+              break
+            case "GS":
+              if (groupCount > 0) {
+                r.errors.push("Missing GE segment at " + i);
+              }
+              isaCount++;
+              groupNum = seg.elements[6].content;
+              break;
+            case "ST":
+              if (transactionSegmentCount > 0) {
+                r.errors.push("missing SE segment at " + i);
+              }
+              groupCount++;
+              transactionSegmentCount = 1;
+              transactionNum          = seg.elements[2].content;
+              break;
+            case "SE":
+              if (transactionNum === "") {
+                r.errors.push("Missing ST for SE at position " + (i + 1));
+              } else if ( transactionNum !== seg.elements[2].content ) {
+                r.errors.push("Wrong transaction control number at position " + (i + 1));
+              }
+              if (seg.elements[1].content != transactionSegmentCount + 1) {
+                r.errors.push("SE at position " + (i + 1) + " is bad (count should be " + transactionSegmentCount + ")");
+              }
+              transactionNum          = "";
+              transactionSegmentCount = 0;
+              break;
+            case "GE":
+              if (transactionNum !== "") {
+                r.errors.push("Missing SE at position " + i);
+                transactionNum = ""
+              }
+              if (groupNum === "") {
+                r.errors.push("Missing GS for GE at position " + (i + 1));
+              } else if ( groupNum !== seg.elements[2].content ) {
+                r.errors.push("Wrong group control number at position " + (i + 1));
+              }
+              if (seg.elements[1].content != groupCount) {
+                r.errors.push("GE at position " + (i + 1) + " is bad (count should be " + groupCount + ")");
+              }
+              groupNum   = "";
+              groupCount = 0;
+              break;
+            case "IEA":
+              if (transactionNum !== "") {
+                r.errors.push("Missing SE at position " + i);
+              }
+              if (groupNum !== "") {
+                r.errors.push("Missing GE at position " + i);
+              }
+              if (isaCount != seg.elements[1].content) {
+                r.errors.push("IEA at positon " + (i + 1) + " is bad (count should be " + isaCount + ")");
+              }
+              if (isaNum !== seg.elements[2].content) {
+                r.errors.push("Wrong interchange control number at position " + (i + 1));
+              }
+              break;
+            default:
+              if (transactionSegmentCount > 0) {
+                transactionSegmentCount++;
+              }
+              break;
+          }
           r.segments.push(seg);
           sTypes[seg.type] = true;
         }
